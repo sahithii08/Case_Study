8a3327cc580462374b2f4d4ac95b324cfba85399
sahithii08 <163488992+sahithii08@users.noreply.github.com>
2025-10-22 01:36:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def home():
     return render_template('index.html', reminders=reminders, due=due, message=message)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
